1d396a0b84ec5c5191e0e2aab8222c4fa9081261
Ömer Faruk Gönen <ofgonen@gmail.com>
2022-03-28 15:39:32 +0300

    Bar: Movement drag is now level data

--- Assets/Script/FFStudio/LevelData.cs
+++ Assets/Script/FFStudio/LevelData.cs
@@ -24,6 +24,7 @@ namespace FFStudio
         [ BoxGroup( "AI" ), MinMaxSlider( 0, 10f ) ] public Vector2 ai_answer_rate;
         [ BoxGroup( "AI" ), Range( 0, 5 ) ]  public float ai_ball_power = 1f;
         [ BoxGroup( "AI" ), Range( 0, 10 ) ] public int ai_ball_health  = 2;
+        [ BoxGroup( "Bar" ) ] public float bar_movement_drag = 1f;
 
 		private Dictionary< int, string > question_answers_dictionary;
 
